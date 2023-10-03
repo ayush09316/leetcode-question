c8ee141c5eae3c5119dd879946b8d0cc7ac612d7
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-04 00:11:40 +0530

    Time: 13 ms (23.49%), Space: 12.6 MB (47.71%) - LeetHub

--- 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
+++ 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> rightmin(n+1,INT_MAX);
-        for(int i=n-1;i>=0;i--){
-            rightmin[i] = min(arr[i],rightmin[i+1]);
+        int n=arr.size();
+
+        vector<int>rightMin(n+1);
+
+        rightMin[n]=INT_MAX;
+        for(int i=n-1; i>=0; i--){
+            rightMin[i]=min( rightMin[i+1], arr[i]);
         }
-        
-        int count = 0;
-        int leftmax = INT_MIN;
-        for(int i=0;i<n;i++){
-            leftmax = max(leftmax,arr[i]);
-            if(leftmax <= rightmin[i+1])  count++;
+
+        int chunks=0;
+        int leftMax=INT_MIN;
+        for(int i=0; i<n; i++){
+            leftMax=max(leftMax, arr[i]);
+
+            if(leftMax <= rightMin[i+1])chunks++;
         }
-        return count++;
+        return chunks;
     }
 };
