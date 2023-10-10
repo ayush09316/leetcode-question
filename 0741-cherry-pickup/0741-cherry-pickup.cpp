d6a5100da4438131c58384232a3539a309db88a8
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-10 22:00:29 +0530

    Time: 26 ms (97.77%), Space: 10 MB (75.99%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    int fun(int r1,int c1,int r2,int n,vector<vector<int>>&grid, vector<vector<vector<int>>> &dp){
-        int c2=r1+c1-r2;  //r1+c1==r2+c2 => c2=r1+c1-r2;
-        if(r1>=n or c1>=n or r2>=n or c2>=n or grid[r1][c1]==-1 or grid[r2][c2]==-1)
-          return -1e7; 
-          
-        if(dp[r1][c1][r2]!=-1) return dp[r1][c1][r2]; 
-     
-        //if one can reach the end then other can also reach the end.
-        if(r1==n-1 and c1==n-1)  return grid[r1][c1];
-        int temp=0;     
-        if(r1==r2 and c1==c2) temp+=grid[r1][c1]; 
-        else temp+=grid[r1][c1]+grid[r2][c2];
+     int dp[51][51][51];
+    int func(int r1 , int c1 , int r2 , int n , vector<vector<int>> &grid )
+    {
+        int c2=r1+c1-r2;
         
-        int a=fun(r1+1,c1,r2+1,n,grid,dp); 
-        int b=fun(r1+1,c1,r2,n,grid,dp);     
-        int c=fun(r1,c1+1,r2,n,grid,dp);          
-        int d=fun(r1,c1+1,r2+1,n,grid,dp);         
+        if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1)
+        {
+            return INT_MIN;
+        }
         
-        temp+=max({a,b,c,d}); 
-        dp[r1][c1][r2]=temp;
-        return temp;
-
+        if(dp[r1][c1][r2]!=-1)
+        {
+            return dp[r1][c1][r2];
+        }
+        
+        if(r1==n-1 && c1==n-1)
+        {
+            return grid[r1][c1];
+        }
+        
+        int ans=grid[r1][c1];
+        
+        if(r1!=r2)
+        {
+            ans += grid[r2][c2];
+        }
+        
+        int temp= max(func(r1+1 , c1 , r2+1 , n , grid) , func(r1 , c1+1 , r2 , n , grid));
+        temp = max(temp , func(r1+1 , c1 , r2 , n, grid));
+        temp = max(temp , func(r1 , c1+1 , r2+1 , n , grid));
+        
+        ans+=temp;
+        
+        return dp[r1][c1][r2]=ans;
     }
     int cherryPickup(vector<vector<int>>& grid) {
+        memset(dp , -1 , sizeof(dp));
         int n=grid.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(n,vector<int>(n,-1)));
-        return max(0,fun(0,0,0,n,grid,dp));
-
+        return max(0 , func(0,0,0,n , grid));
     }
 };
+   
