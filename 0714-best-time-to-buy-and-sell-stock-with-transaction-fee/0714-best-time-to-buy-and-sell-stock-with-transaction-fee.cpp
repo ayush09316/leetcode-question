08d2989d3dd6689237855261250858b395b1c47e
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-11 10:35:56 +0530

    Time: 73 ms (91.38%), Space: 55.3 MB (75.06%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    int solve(vector<int>& prices,int fee,int index,int buy,vector<vector<int>>&dp){
-        if(index==prices.size()) return 0;
-        if(dp[index][buy]!=-1) return dp[index][buy];
-        int profit=0;
-        if(buy){
-            int buyIt= solve(prices,fee,index+1,0,dp)-prices[index];
-            int notBuy=solve(prices,fee,index+1,1,dp);
-            profit=max(notBuy,buyIt);
-        }else{
-            int sellIt= solve(prices,fee,index+1,1,dp)+prices[index]-fee;
-            int notSell=solve(prices,fee,index+1,0,dp);
-            profit=max(notSell,sellIt);
-        }
-        return dp[index][buy]=profit;
-    }
     int maxProfit(vector<int>& prices, int fee) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return solve(prices,fee,0,1,dp);
+       int currentBuy = INT_MIN;
+        int currentSell = 0;
+        for (int price : prices)
+        {
+            currentBuy = max(currentBuy, currentSell - price);
+            currentSell = max(currentSell, currentBuy + price - fee);
+        }
+
+        return currentSell;
     }
 };
