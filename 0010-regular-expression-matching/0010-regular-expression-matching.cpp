19ceff6abbada9ca38fe03be6ee54baf497b3d19
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 11:15:22 +0530

    Time: 0 ms (100%), Space: 7.4 MB (20.97%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,8 +1,30 @@
 class Solution {
 public:
      bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length()+1,vector<int>(p.length(),-1));
-        return helper(s,p,0,0,dp);
+       // return helper(s,p,0,0,dp);
+       return tab(s,p);
+    }
+
+    bool tab(string s, string p){
+        vector<vector<int>> dp(s.length()+1,vector<int>(p.length()+1,0));
+        dp[s.length()][p.length()]=1;
+        
+        for(int i=s.length();i>=0;i--)
+        {
+            for(int j=p.length()-1;j>=0;j--)
+            {
+                bool first_match=(i<s.length() && (p[j]==s[i]|| p[j]=='.'));
+                    if(j+1<p.length() && p[j+1]=='*')
+                    {
+                        dp[i][j]=dp[i][j+2] || (first_match && dp[i+1][j]);
+                    }
+                else
+                {
+                    dp[i][j]=first_match && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
     
     bool helper(string s, string p, int i, int j,vector<vector<int>> &dp){
