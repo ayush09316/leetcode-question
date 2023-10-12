51c02d122fce40e1c89b9bfca6f9f66c6cf6e0d3
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 19:21:34 +0530

    Time: 1539 ms (18.72%), Space: 6.8 MB (57.29%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,29 +1,39 @@
 class Solution {
 private:
-    int n; int memo[2000];
     bool isPalindrome(string& s, int l, int r){
         while(l < r){
             if(s[l++] != s[r--])  return false;
         }
         return true;
     }
-    int solve(string& s, int idx){
-        if(idx >= n)  return 0;
-        if(memo[idx] != -1) return memo[idx];
+    //  int solve(string& s, int idx){
+    //     if(idx >= n)  return 0;
+    //     if(memo[idx] != -1) return memo[idx];
         
-        int minSteps = INT_MAX;
-        for(int k=idx; k<n; k++){
-            if(isPalindrome(s, idx, k)){
-                int steps = 1 + solve(s, k+1);
-                minSteps = min(minSteps, steps);
-            }
-        }
-        return memo[idx]=minSteps;
-    }
+    //     int minSteps = INT_MAX;
+    //     for(int k=idx; k<n; k++){
+    //         if(isPalindrome(s, idx, k)){
+    //             int steps = 1 + solve(s, k+1);
+    //             minSteps = min(minSteps, steps);
+    //         }
+    //     }
+    //     return memo[idx]=minSteps;
+    // }
 public:
     int minCut(string s) {
-        n = s.size();
-        memset(memo, -1, sizeof(memo));
-        return solve(s, 0) - 1;
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        //return solve(s, 0) - 1;
+        for(int i=n-1; i>=0; i--){
+            int minSteps = INT_MAX;
+            for(int k=i; k<n; k++){
+                if(isPalindrome(s, i, k)){
+                    int steps = 1 + dp[k+1];
+                    minSteps = min(minSteps, steps);
+                }
+            }
+            dp[i] = minSteps;
+        }
+        return dp[0] - 1;
     }
 };
