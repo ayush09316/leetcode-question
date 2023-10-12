d9279ad579affb2bc0d3f4ae74b37760767c974b
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 10:42:46 +0530

    Time: 13 ms (46.09%), Space: 9.5 MB (25.21%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -18,8 +18,71 @@ public:
          
 
     }
-    int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.length(),vector<int>(word2.length(),-1));
-        return solve(word1,word2,0,0,dp);
+    int solveTab(string &a,string &b){
+        vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
+
+        //step2 : analyse base case
+        for(int j=0;j<=b.length();j++){
+            dp[a.length()][j] = b.length()-j;
+        }
+        for(int i=0;i<=a.length();i++){
+            dp[i][b.length()] = a.length()-i;
+        }
+
+        //step 3: Using Bottom Up Approach Calculate answer using Loops
+        for(int i=a.length()-1;i>=0;i--){
+            for(int j=b.length()-1;j>=0;j--){
+                int ans = 0;
+                if(a[i] == b[j])  ans = dp[i+1][j+1];
+                else{
+                    int insert = dp[i][j+1];
+                    int deleteAns = dp[i+1][j];
+                    int replace = dp[i+1][j+1];
+                    ans = 1 + min(insert,min(deleteAns,replace));
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+    }
+    int solveTabSO(string &a,string &b){
+        //step1 : create dp array
+        // vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
+        vector<int>curr(b.length()+1,0);
+        vector<int>next(b.length()+1,0);
+        //step2 : analyse base case
+        //TODO
+        for(int j=0;j<=b.length();j++){
+            next[j] = b.length()-j;
+        }
+
+        //step 3: Using Bottom Up Approach Calculate answer using Loops
+        for(int i=a.length()-1;i>=0;i--)
+        {
+            curr[b.length()] = a.length()-i;
+            for(int j=b.length()-1;j>=0;j--)
+            {
+                int ans = 0;
+                if(a[i] == b[j]){
+                    ans = next[j+1];
+                }
+                else{
+                    //insert 
+                    int insert = curr[j+1];
+                    //delete
+                    int deleteAns = next[j];
+                    //replace
+                    int replace= next[j+1];
+                    ans = 1 + min(insert,min(deleteAns,replace));
+                }
+                curr[j]=ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    int minDistance(string a, string b) {
+        //return solve(word1,word2,0,0,dp);
+        return solveTab(a,b);
     }
 };
