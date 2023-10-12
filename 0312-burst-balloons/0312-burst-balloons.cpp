cd3403f63d983e149f05710d0681bec9e5214592
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 12:06:32 +0530

    Time: 249 ms (84.82%), Space: 10.5 MB (30.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -13,12 +13,29 @@ public:
         }
         return dp[i][j]=maxi;
     }
+    int tab(vector<int>& nums,int n){
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j) continue;
+                int maxi=INT_MIN;
+                for(int k=i;k<=j;k++){
+                    int cost= nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi=max(maxi,cost);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n];
+    }
 
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(nums,1,n,dp);
+       // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+       // return solve(nums,1,n,dp);
+       return tab(nums,n);
+       
     }
 };
