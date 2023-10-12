c704da8fc606af449e8406b8b421fc5446f92ebf
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 10:32:55 +0530

    Time: 39 ms (36.22%), Space: 8.5 MB (65.05%) - LeetHub

--- 3033-apply-operations-to-make-two-strings-equal/3033-apply-operations-to-make-two-strings-equal.cpp
+++ 3033-apply-operations-to-make-two-strings-equal/3033-apply-operations-to-make-two-strings-equal.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
 int dp[501][501];
     int cal(int i , int j ,int x,vector<int>&arr){
-        if(i>j)
-            return 0;
-            if(dp[i][j]!=-1)
-                return dp[i][j];
+        if(i>j) return 0;
+
+        if(dp[i][j]!=-1)  return dp[i][j];
         int a ,b,c;
         a = min(arr[i+1]-arr[i] , x) + cal(i+2 , j , x,arr);
         b = min(arr[j]-arr[i] , x) + cal(i+1 , j-1 , x ,arr);
@@ -13,13 +12,12 @@ int dp[501][501];
 
         return dp[i][j] = min({a,b,c});
     }
-    
+
     int minOperations(string s1, string s2, int x) {
         int n = s1.length();
         vector<int> arr;
         for(int  i= 0 ;  i< n ; i++){
-            if(s1[i]!=s2[i])
-                arr.push_back(i);
+            if(s1[i]!=s2[i])  arr.push_back(i);
         }
 
         if(arr.size()%2 == 1)  return -1;
