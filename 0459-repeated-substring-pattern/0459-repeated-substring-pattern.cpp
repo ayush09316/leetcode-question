ead9b6b61316138400323ecbc281fc3d1ae153dd
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-12 22:55:36 +0530

    Time: 21 ms (59.88%), Space: 14.1 MB (52.16%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n=s.size();
-        for(int i=n/2;i>=1;i--){               
-            if(n%i==0){                                                            
-                if(s.substr(0,n-i)==s.substr(i)) return true;           
-            }
-        }
-        return false;  
+         string temp = s + s;
+        return temp.substr(1, s.length() * 2 - 2).find(s) != -1;
     }
 };
