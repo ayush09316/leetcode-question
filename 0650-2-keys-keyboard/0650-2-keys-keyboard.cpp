ab9600e09f0a7163663c2d197f226678bea761bd
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-28 18:29:49 +0530

    Time: 2 ms (81.73%), Space: 6.4 MB (60.46%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int f(int cur, int prev, int tar, vector<vector<int>> &dp) {
-    if (cur == tar) return 0;
-    if (cur + prev > tar) return 2000;
-    
-    if (dp[cur][prev] != -1) return dp[cur][prev];
-
-    int cp = 2 + f(2*cur, cur, tar, dp);
-    int p = 1 + f(cur + prev, prev, tar, dp);
-
-    return dp[cur][prev] = min(cp, p);
-    }
 
     int minSteps(int n) {
-        if (n == 1) return 0;
-        vector<vector<int>> dp(n + 1, vector<int>(n, -1));
-        return f(2, 1, n, dp) + 2;
+       int steps = 0;
+        for (int i = 2; i <= n; i++) {
+            while (n %i == 0) {
+                steps += i;
+                n /= i;
+            }
+        }
+        return steps;
     }
 };
