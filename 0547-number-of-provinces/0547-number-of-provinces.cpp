30b3d150daf15b071798ee793a9e38733f6da14e
Ayush Sharma <ayushsharma170199@gmail.com>
2023-10-30 11:20:37 +0530

    Time: 23 ms (55.03%), Space: 15.7 MB (75.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     vector<int>parent;
     int findParent(int x){
-        return (parent[x]==x)? x: findParent(parent[x]);
+        if (parent[x]==-1) return x; 
+        return parent[x]=findParent(parent[x]);
+    }
+    void _union(int a,int b){
+        int p_a=findParent(a) , p_b=findParent(b);
+        if(p_a==p_b) return; 
+        parent[p_a]=p_b;
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        int m = isConnected[0].size();
-        int ans = n;
- 		parent.resize(n+1, 0);
- 		for (int i = 0; i <= n; i++)   parent[i] = i;
+ 		parent.resize(n+1, -1);
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            for(int j=0;j<n;j++){
                 if(isConnected[i][j] && i!=j){
-                    int x= findParent(i+1);
-                    int y= findParent(j+1);
-                    if(x!=y){
-                        parent[x]=y;
-                        ans--;
-                    }
-
+                    _union(i,j);
                 }
             }
         }
-        return ans;
+        
+        int c=0; 
+        for(int i=0;i<n;i++){
+            if(parent[i]==-1) c++; 
+        }
+
+        return c; 
     }
 };
