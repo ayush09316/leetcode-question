3ca335b8738d03d9f8361603f37cbf8a99302193
Ayush Sharma <ayushsharma170199@gmail.com>
2023-11-03 19:08:26 +0530

    Time: 38 ms (49.04%), Space: 22.5 MB (60.04%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool solve(int mid,int th, vector<int>&nums){
+        int sum=0;
+       for(int num : nums){
+        sum+= ceil((double)(num)/(double)(mid));
+       }
+       return sum<=th;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int low=1 , high=nums[n-1];
+        while(low<=high){
+            int mid= low + (high-low)/2;
+            if(solve(mid,threshold,nums)) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
