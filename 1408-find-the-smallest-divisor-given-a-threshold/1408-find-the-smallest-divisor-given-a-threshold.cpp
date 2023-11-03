587602323c1219752b67d72fae4b4b4f0f8554d5
Ayush Sharma <ayushsharma170199@gmail.com>
2023-11-03 19:11:19 +0530

    Time: 47 ms (8.58%), Space: 22.5 MB (60.04%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -11,9 +11,9 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         int low=1 , high=nums[n-1];
-        while(low<=high){
+        while(low<high){
             int mid= low + (high-low)/2;
-            if(solve(mid,threshold,nums)) high=mid-1;
+            if(solve(mid,threshold,nums)) high=mid;
             else low=mid+1;
         }
         return low;
