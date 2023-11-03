ace1e5533890d9bfeb2782224cf2c35c1be6f82f
Ayush Sharma <ayushsharma170199@gmail.com>
2023-11-03 13:16:40 +0530

    Time: 8 ms (84.11%), Space: 10.4 MB (63.54%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int check(vector<int>& a,int x,int k){
+    bool check(vector<int>& a,int x,int k){
         int cnt=0,n=a.size(),ind=0;
         for(int i=1;i<n;i++){
             while(ind<i && (a[i]-a[ind]>x))   ind++;
             cnt+=(i-ind);
         }
-        return (cnt>=k)? -1: 1;
+        return (cnt>=k)? 1: 0;
     
     }
     int smallestDistancePair(vector<int>& a, int k) {
         sort(a.begin(),a.end());
         int n=a.size();
-        int l=0,h=a[n-1]-a[0];
+        int l=0,  h=a[n-1]-a[0];
         while(l<h){
             int mid=l+(h-l)/2;
-            int flag=check(a,mid,k);
-            if(flag==-1)  h=mid;
+            if(check(a,mid,k))  h=mid;
             else  l=mid+1;
         }
         return l;
